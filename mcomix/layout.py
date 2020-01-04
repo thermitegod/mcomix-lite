1c47a54d76c44c5ee8371bcc60b41125ae5bc340
thermitegod <thermitethegod@gmail.com>
2020-01-03 19:52:27 -0700

    move remaining scrolling code to layout

--- mcomix/layout.py
+++ mcomix/layout.py
@@ -2,7 +2,7 @@
 
 """Layout"""
 
-from mcomix import box, constants, scrolling
+from mcomix import box, constants
 
 
 class FiniteLayout(object):  # 2D only
@@ -18,7 +18,6 @@ class FiniteLayout(object):  # 2D only
         content boxes.
         @param distribution_axis: the axis along which the Boxes are distributed.
         @param alignment_axis: the axis to center"""
-        self.scroller = scrolling.Scrolling()
         self.current_index = -1
         self.wrap_individually = wrap_individually
         self._reset(content_sizes, viewport_size, orientation, spacing,
@@ -56,8 +55,48 @@ class FiniteLayout(object):  # 2D only
             if index == constants.LAST_INDEX:
                 index = len(self.content_boxes) - 1
             current_box = self.wrapper_boxes[index]
-        self.set_viewport_position(self.scroller.scroll_to_predefined(
+        self.set_viewport_position(self._scroll_to_predefined(
                 current_box, self.viewport_box, self.orientation, destination))
+
+    @staticmethod
+    def _scroll_to_predefined(content_box, viewport_box, orientation, destination):
+        """Returns a new viewport position when scrolling towards a
+        predefined destination. Note that all params are lists of integers
+        where each index corresponds to one dimension.
+        @param content_box: The Box of the content to display.
+        @param viewport_box: The viewport Box we are looking through.
+        @param orientation: The orientation which shows where "forward"
+        points to. Either 1 (towards larger values in this dimension when
+        reading) or -1 (towards smaller values in this dimension when reading).
+        @param destination: An integer representing a predefined destination.
+        Either 1 (towards the greatest possible values in this dimension),
+        -1 (towards the smallest value in this dimension), 0 (keep position),
+        SCROLL_TO_CENTER (scroll to the center of the content in this
+        dimension), SCROLL_TO_START (scroll to where the content starts in this
+        dimension) or SCROLL_TO_END (scroll to where the content ends in this dimension).
+        @return: A new viewport position as specified above"""
+        content_position = content_box.get_position()
+        content_size = content_box.get_size()
+        viewport_size = viewport_box.get_size()
+        result = list(viewport_box.get_position())
+        for i in range(len(content_size)):
+            o = orientation[i]
+            d = destination[i]
+            if d == 0:
+                continue
+            if d < constants.SCROLL_TO_END or d > 1:
+                raise ValueError(f'invalid destination {d} at index {i}')
+            if d == constants.SCROLL_TO_END:
+                d = o
+            if d == constants.SCROLL_TO_START:
+                d = -o
+            c = content_size[i]
+            v = viewport_size[i]
+            invisible_size = c - v
+            result[i] = content_position[i] + (box.Box._box_to_center_offset_1d(invisible_size, o)
+                                               if d == constants.SCROLL_TO_CENTER
+                                               else invisible_size if d == 1 else 0)  # if d == -1
+        return result
 
     def get_content_boxes(self):
         """Returns the Boxes as they are arranged in this layout.
