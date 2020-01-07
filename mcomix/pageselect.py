thermitegod/mcomix-lite

commit 94f4fd0e5fd5034f335a61db2b1b88e5a63cfae3

multithreading and others most changes come from between https://github.com/multiSnow/mcomix3/commit/0743e34b3fb5374ac4c6006caf43ca9b1f8c833c and https://github.com/multiSnow/mcomix3/commit/c4ca4bac4f4f357ebc066aba1c5c91b95b813d84 currently there is a problem with the extractor in archive_recursive.py ~155 where it can leave tmpfiles behind. need to look into changing tmpfile handeling

mcomix/pageselect.py
@@ -4,13 +4,13 @@
 
 from gi.repository import Gtk
 
-from mcomix import callback
+from mcomix import callback, mt
 from mcomix.preferences import prefs
-from mcomix.worker_thread import WorkerThread
 
 
 class Pageselector(Gtk.Dialog):
     """The Pageselector takes care of the popup page selector"""
+
     def __init__(self, window):
         self._window = window
         super(Pageselector, self).__init__(title='Go to page...', modal=True, destroy_with_parent=True)
@@ -68,7 +68,7 @@ class Pageselector(Gtk.Dialog):
 
         # Currently displayed thumbnail page.
         self._thumbnail_page = 0
-        self._thread = WorkerThread(self._generate_thumbnail, name='preview')
+        self._thread = mt.ThreadPool(name=self.__class__.__name__)
         self._update_thumbnail(int(self._selector_adjustment.props.value))
         self._window.imagehandler.page_available += self._page_available
 
@@ -100,7 +100,7 @@ class Pageselector(Gtk.Dialog):
             self._window.set_page(int(self._selector_adjustment.props.value))
 
         self._window.imagehandler.page_available -= self._page_available
-        self._thread.stop()
+        self._thread.renew()
         self.destroy()
 
     def _update_thumbnail(self, page):
@@ -108,16 +108,19 @@ class Pageselector(Gtk.Dialog):
         width = self._image_preview.get_allocation().width
         height = self._image_preview.get_allocation().height
         self._thumbnail_page = page
-        self._thread.clear_orders()
-        self._thread.append_order((page, width, height))
+        self._thread.apply_async(
+                self._generate_thumbnail, args=(page, width, height),
+                callback=self._generate_thumbnail_cb)
 
     def _generate_thumbnail(self, params):
         """Generate the preview thumbnail for the page selector.
         A transparent image will be used if the page is not yet available"""
-        page, width, height = params
+        return page, self._window.imagehandler.get_thumbnail(
+                page, width=width, height=height, nowait=True)
 
-        pixbuf = self._window.imagehandler.get_thumbnail(page, width=width, height=height, nowait=True)
-        self._thumbnail_finished(page, pixbuf)
+    def _generate_thumbnail_cb(self, params):
+        page, pixbuf = params
+        return self._thumbnail_finished(page, pixbuf)
 
     @callback.Callback
     def _thumbnail_finished(self, page, pixbuf):

mcomix-authors
@@ -1,54 +1,0 @@
-AUTHORS
-Pontus Ekberg Original vision/developer of Comix
-Louis Casillas MComix developer
-Moritz Brunner MComix developer
-Ark MComix developer
-Benoit Pierre MComix developer
-
-
-TRANSLATORS
-Emfox Zho Simplified Chinese translation
-Xie Yanbo Simplified Chinese translation
-Zach Cheung Simplified Chinese translation
-Manuel Quiñones Spanish translation
-Carlos Feli Spanish translation
-Marcelo Góes Brazilian Portuguese translation
-Christoph Wolk German translation and Nautilus thumbnailer
-Chris Leick German translation
-Raimondo Giammanco Italian translation
-Giovanni Scafora Italian translation
-GhePe Italian translation
-Arthur Nieuwland Dutch translation
-Achraf Cherti French translation
-Benoît H. French translation
-Joseph M. Sleiman French translation
-Frédéric Chateaux French translation
-Kamil Leduchowski Polish translatin
-Darek Jakoniuk Polish translation
-Paul Chatzidimitrio Greek translation
-Carles Escrig Royo Catalan translation
-Hsin-Lin Cheng Traditional Chinese translation
-Wayne S Traditional Chinese translation
-Mamoru Tasaka Japanese translation
-Keita Haga Japanese translation
-Toshiharu Kudoh Japanese translation
-Ernő Drabik Hungarian translation
-Artyom Smirnov Russian translation
-Евгений Лежнин Russian translation
-Adrian C. Croatian translation
-김민기 Korean translation
-Gyeongmin Bak Korean translation
-Minho Jeung Korean translation
-Maryam Sanaat Persian translation
-Andhika Padmawan Indonesian translation
-Jan Nekvasil Czech translation
-Олександр Заяц Ukrainian translation
-Roxerio Roxo Carrillo Galician translation
-Martin Karlsson Swedish translation
-Jonatan Nyberg Swedish translation
-Isratine Citizen Hebrew translation
-Zygi Mantus Lithuanian translation
-
-
-ARTISTS
-Victor Castillejo Icon design

mcomix/about_dialog.py
@@ -19,7 +19,7 @@ class _AboutDialog(Gtk.AboutDialog):
         self.set_name(constants.APPNAME)
         self.set_program_name(constants.APPNAME)
         self.set_version('Version ' + constants.VERSION)
-        self.set_license_type(Gtk.License.GPL_2_0,)
+        self.set_license_type(Gtk.License.GPL_2_0, )
         self.set_website('https://github.com/thermitegod/mcomix-lite')
         self.set_website_label('Github')
         self.set_copyright('Copyright (C) 2005-2016')

mcomix/archive/__init__.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+
+from gi.repository import Gtk
+
+from mcomix import main, message_dialog
+
+
+def ask_for_password(archive):
+    """Openes an input dialog to ask for a password. Returns either
+    an Unicode string (the password), or None"""
+    dialog = message_dialog.MessageDialog(
+            main.main_window(),
+            flags=Gtk.DialogFlags.MODAL,
+            message_type=Gtk.MessageType.QUESTION,
+            buttons=Gtk.ButtonsType.OK_CANCEL)
+    dialog.set_text('The archive is password-protected:',
+                    archive + '\n\n' + 'Please enter the password to continue:')
+    dialog.set_default_response(Gtk.ResponseType.OK)
+    dialog.set_auto_destroy(False)
+
+    password_box = Gtk.Entry()
+    password_box.set_visibility(False)
+    password_box.set_activates_default(True)
+    dialog.get_content_area().pack_end(password_box, True, True, 0)
+    dialog.set_focus(password_box)
+
+    result = dialog.run()
+    password = password_box.get_text()
+    dialog.destroy()
+
+    if result == Gtk.ResponseType.OK and password:
+        return password
+    else:
+        return None
+
+#

mcomix/archive/archive_base.py
@@ -7,7 +7,7 @@ import errno
 import os
 import threading
 
-from mcomix import process
+from mcomix import archive, callback, process
 
 
 class BaseArchive(object):
@@ -22,11 +22,18 @@ class BaseArchive(object):
         assert isinstance(archive, str), 'File should be an Unicode string.'
 
         self.archive = archive
+        self._password = None
+        self.is_encrypted = False
         self._event = threading.Event()
+        if self.support_concurrent_extractions:
+            # When multiple concurrent extractions are supported,
+            # we need a lock to handle concurent calls to _get_password.
+            self._lock = threading.Lock()
+            self._waiting_for_password = False
 
     def iter_contents(self):
         """Generator for listing the archive contents"""
-        return
+        # return
         yield
 
     def list_contents(self):
@@ -34,15 +41,14 @@ class BaseArchive(object):
         These names do not necessarily exist in the actual archive since they
         need to saveable on the local filesystems, so some characters might
         need to be replaced"""
-
-        return [f for f in self.iter_contents()]
+        return [filename for filename in self.iter_contents()]
 
     def extract(self, filename, destination_dir):
         """Extracts the file specified by <filename> and return the path of it.
         This filename must be obtained by calling list_contents().
         The file is saved to <destination_dir>."""
-
-        assert isinstance(filename, str) and isinstance(destination_dir, str)
+        assert isinstance(filename, str) and \
+               isinstance(destination_dir, str)
         return os.path.join(destination_dir, filename)
 
     def iter_extract(self, entries, destination_dir):
@@ -54,7 +60,7 @@ class BaseArchive(object):
             self.extract(filename, destination_dir)
             yield filename
             wanted.remove(filename)
-            if 0 == len(wanted):
+            if not wanted:
                 break
 
     def close(self):
@@ -79,11 +85,37 @@ class BaseArchive(object):
                 raise e
 
     def _create_file(self, dst_path):
-        """Open <dst_path> for writing, making sure base directory exists"""
+        """ Open <dst_path> for writing, making sure base directory exists. """
         dst_dir = os.path.dirname(dst_path)
         # Create directory if it doesn't exist
         self._create_directory(dst_dir)
         return open(dst_path, 'wb')
+
+    @callback.Callback
+    def _password_required(self):
+        """Asks the user for a password and sets <self._password>.
+        If <self._password> is None, no password has been requested yet.
+        If an empty string is set, assume that the user did not provide
+        a password"""
+        password = archive.ask_for_password(self.archive)
+        if password is None:
+            password = ''
+
+        self._password = password
+        self._event.set()
+
+    def _get_password(self):
+        ask_for_password = self._password is None
+        # Don't trigger concurrent password dialogs.
+        if ask_for_password and self.support_concurrent_extractions:
+            with self._lock:
+                if self._waiting_for_password:
+                    ask_for_password = False
+                else:
+                    self._waiting_for_password = True
+        if ask_for_password:
+            self._password_required()
+        self._event.wait()
 
 
 class ExternalExecutableArchive(BaseArchive):
@@ -136,7 +168,8 @@ class ExternalExecutableArchive(BaseArchive):
 
     def extract(self, filename, destination_dir):
         """Extract <filename> from the archive to <destination_dir>"""
-        assert isinstance(filename, str) and isinstance(destination_dir, str)
+        assert isinstance(filename, str) and \
+               isinstance(destination_dir, str)
 
         if not self._get_executable():
             return

mcomix/archive/archive_recursive.py
@@ -14,7 +14,9 @@ class RecursiveArchive(archive_base.BaseArchive):
     def __init__(self, archive, prefix='mcomix.'):
         super(RecursiveArchive, self).__init__(archive.archive)
         self._main_archive = archive
-        self._tempdir = tempfile.TemporaryDirectory(prefix=prefix, dir=prefs['temporary directory'])
+        self.is_encrypted = self._main_archive.is_encrypted
+        self._tempdir = tempfile.TemporaryDirectory(
+                prefix=prefix, dir=prefs['temporary directory'])
         self._sub_tempdirs = []
         self.destdir = self._tempdir.name
         self._archive_list = []
@@ -27,7 +29,9 @@ class RecursiveArchive(archive_base.BaseArchive):
         # Assume concurrent extractions are not supported.
         self.support_concurrent_extractions = False
 
-    def _iter_contents(self, archive, root=None):
+    def _iter_contents(self, archive, root=None, decrypt=True):
+        if archive.is_encrypted and not decrypt:
+            return
         if not root:
             root = os.path.join(self.destdir, 'main_archive')
         self._archive_list.append(archive)
@@ -54,10 +58,12 @@ class RecursiveArchive(archive_base.BaseArchive):
             # And open it and list its contents.
             sub_archive = archive_tools.get_archive_handler(sub_archive_path)
             if sub_archive is None:
-                log.warning('Non-supported archive format: %s', os.path.basename(sub_archive_path))
+                log.warning('Non-supported archive format: %s',
+                            os.path.basename(sub_archive_path))
                 continue
             sub_tempdir = tempfile.TemporaryDirectory(
-                prefix='sub_archive.{:04}.'.format(len(self._archive_list)), dir=self.destdir)
+                    prefix='sub_archive.{:04}.'.format(len(self._archive_list)),
+                    dir=self.destdir)
             sub_root = sub_tempdir.name
             self._sub_tempdirs.append(sub_tempdir)
             for name in self._iter_contents(sub_archive, sub_root):
@@ -72,23 +78,23 @@ class RecursiveArchive(archive_base.BaseArchive):
                 break
         self.support_concurrent_extractions = supported
 
-    def iter_contents(self):
+    def iter_contents(self, decrypt=True):
         if self._contents_listed:
             for f in self._contents:
                 yield f
             return
         self._contents = []
-        for f in self._iter_contents(self._main_archive):
+        for f in self._iter_contents(self._main_archive, decrypt=decrypt):
             self._contents.append(f)
             yield f
         self._contents_listed = True
         # We can now check if concurrent extractions are really supported.
         self._check_concurrent_extraction_support()
 
-    def list_contents(self):
+    def list_contents(self, decrypt=True):
         if self._contents_listed:
             return self._contents
-        return [f for f in self.iter_contents()]
+        return [f for f in self.iter_contents(decrypt=decrypt)]
 
     def extract(self, filename):
         if not self._contents_listed:
@@ -98,7 +104,8 @@ class RecursiveArchive(archive_base.BaseArchive):
         destination_dir = self.destdir
         if root is not None:
             destination_dir = os.path.join(destination_dir, root)
-        log.debug('extracting from %s to %s: %s', archive.archive, destination_dir, filename)
+        log.debug('extracting from %s to %s: %s',
+                  archive.archive, destination_dir, filename)
         return archive.extract(name, destination_dir)
 
     def iter_extract(self, entries, destination_dir):
@@ -121,7 +128,8 @@ class RecursiveArchive(archive_base.BaseArchive):
             if root is not None:
                 archive_destination_dir = os.path.join(destination_dir, root)
             log.debug('extracting from %s to %s: %s',
-                      archive.archive, archive_destination_dir, ' '.join(archive_wanted.keys()))
+                      archive.archive, archive_destination_dir,
+                      ' '.join(archive_wanted.keys()))
             for f in archive.iter_extract(archive_wanted.keys(), archive_destination_dir):
                 yield archive_wanted[f]
             wanted -= set(archive_wanted.values())
@@ -143,7 +151,11 @@ class RecursiveArchive(archive_base.BaseArchive):
             archive.close()
         for tempdir in self._sub_tempdirs:
             tempdir.cleanup()
-        self._tempdir.cleanup()
+
+        try:
+            self._tempdir.cleanup()
+        except OSError:
+            pass
 
     def __enter__(self):
         return self

mcomix/archive/pdf.py
@@ -16,9 +16,9 @@ PDF_RENDER_DPI_DEF = 72 * 4
 PDF_RENDER_DPI_MAX = 72 * 10
 
 _pdf_possible = None
-_mutool_exec = None
-_mudraw_exec = None
-_mudraw_trace_args = None
+_mutool_exec = []
+_mudraw_exec = []
+_mudraw_trace_args = []
 
 
 class PdfArchive(archive_base.BaseArchive):
@@ -40,7 +40,7 @@ class PdfArchive(archive_base.BaseArchive):
     def extract(self, filename, destination_dir):
         self._create_directory(destination_dir)
         destination_path = os.path.join(destination_dir, filename)
-        page_num = int(filename[0:-4])
+        page_num, ext = os.path.splitext(filename)
         # Try to find optimal DPI.
         cmd = _mudraw_exec + _mudraw_trace_args + ['--', self.archive, str(page_num)]
         log.debug('finding optimal DPI for %s: %s', filename, ' '.join(cmd))
@@ -75,14 +75,16 @@ class PdfArchive(archive_base.BaseArchive):
         global _pdf_possible
         if _pdf_possible is not None:
             return _pdf_possible
-        global _mutool_exec, _mudraw_exec, _mudraw_trace_args
         mutool = process.find_executable('mutool')
         _pdf_possible = False
         version = None
         if mutool is None:
             log.debug('mutool executable not found')
         else:
-            _mutool_exec = [mutool]
+            _mutool_exec.append(mutool)
+            # Find MuPDF version; assume 1.6 version since
+            # the '-v' switch is only supported from 1.7 onward...
+            version = '1.6'
             with process.popen([mutool, '-v'],
                                stdout=process.NULL,
                                stderr=process.PIPE,
@@ -91,10 +93,23 @@ class PdfArchive(archive_base.BaseArchive):
                 if output.startswith('mutool version '):
                     version = output[15:].rstrip()
             version = LooseVersion(version)
-            _mudraw_exec = [mutool, 'draw']
-            _mudraw_trace_args = ['-F', 'trace']
-            _pdf_possible = True
-
+            if version >= LooseVersion('1.8'):
+                # Mutool executable with draw support.
+                _mudraw_exec.extend((mutool, 'draw', '-q'))
+                _mudraw_trace_args.extend(('-F', 'trace'))
+                _pdf_possible = True
+            else:
+                # Separate mudraw executable.
+                mudraw = process.find_executable('mudraw')
+                if mudraw is None:
+                    log.debug('mudraw executable not found')
+                else:
+                    _mudraw_exec.append(mudraw)
+                    if version >= LooseVersion('1.7'):
+                        _mudraw_trace_args.extend(('-F', 'trace'))
+                    else:
+                        _mudraw_trace_args.append('-x')
+                    _pdf_possible = True
         if _pdf_possible:
             log.info('Using MuPDF version: %s', version)
             log.debug('mutool: %s', ' '.join(_mutool_exec))

mcomix/archive/sevenzip.py
@@ -16,16 +16,31 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
     """7z file extractor using the 7z executable"""
     STATE_HEADER, STATE_LISTING, STATE_FOOTER = 1, 2, 3
 
+    class EncryptedHeader(Exception):
+        pass
+
     def __init__(self, archive):
         super(SevenZipArchive, self).__init__(archive)
         self._is_solid = False
         self._contents = []
 
+        self.is_encrypted = False
+        self.is_encrypted = self._has_encryption()
+
     def _get_executable(self):
         return SevenZipArchive._find_7z_executable()
 
+    def _get_password_argument(self):
+        if self.is_encrypted:
+            self._get_password()
+            return '-p' + self._password
+        else:
+            # Add an empty password anyway, to prevent deadlock on reading for
+            # input if we did not correctly detect the archive is encrypted.
+            return '-p'
+
     def _get_list_arguments(self):
-        args = [self._get_executable(), 'l', '-slt']
+        args = [self._get_executable(), 'l', '-slt', self._get_password_argument()]
         args.extend(('--', self.archive))
         return args
 
@@ -33,13 +48,15 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
         args = [self._get_executable(), 'x', '-so']
         if list_file is not None:
             args.append('-i@' + list_file)
+        args.append(self._get_password_argument())
         args.extend(('--', self.archive))
         return args
 
     def _parse_list_output_line(self, line):
-        """Start parsing after the first delimiter (bunch of - characters),
+        """ Start parsing after the first delimiter (bunch of - characters),
         and end when delimiters appear again. Format:
         Date <space> Time <space> Attr <space> Size <space> Compressed <space> Name"""
+
         if line.startswith('----------'):
             if self._state == self.STATE_HEADER:
                 # First delimiter reached, start reading from next line.
@@ -51,6 +68,9 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
             return None
 
         if self._state == self.STATE_HEADER:
+            if (line.startswith('Error:') or line.startswith('ERROR:')) and \
+                    line.endswith(': Can not open encrypted archive. Wrong password?'):
+                raise self.EncryptedHeader()
             if 'Solid = +' == line:
                 self._is_solid = True
 
@@ -65,6 +85,17 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
 
         return None
 
+    def _has_encryption(self):
+        with process.popen(self._get_list_arguments(),
+                           stderr=process.STDOUT,
+                           universal_newlines=True) as proc:
+            for line in proc.stdout:
+                if line.startswith('Encrypted = +'):
+                    return True
+                if 'Can not open encrypted archive. Wrong password' in line:
+                    return True
+        return False
+
     def is_solid(self):
         return self._is_solid
 
@@ -72,21 +103,33 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
         if not self._get_executable():
             return
 
-        #: Indicates which part of the file listing has been read.
-        self._state = self.STATE_HEADER
-        #: Current path while listing contents.
-        self._path = None
-        with process.popen(self._get_list_arguments(), stderr=process.STDOUT, universal_newlines=True) as proc:
-            for line in proc.stdout:
-                filename = self._parse_list_output_line(line.rstrip(os.linesep))
-                if filename is not None:
-                    yield filename
+        # We'll try at most 2 times:
+        # - the first time without a password
+        # - a second time with a password if the header is encrypted
+        for retry_count in range(2):
+            #: Indicates which part of the file listing has been read.
+            self._state = self.STATE_HEADER
+            #: Current path while listing contents.
+            self._path = None
+            with process.popen(self._get_list_arguments(), stderr=process.STDOUT, universal_newlines=True) as proc:
+                try:
+                    for line in proc.stdout:
+                        filename = self._parse_list_output_line(line.rstrip(os.linesep))
+                        if filename is not None:
+                            yield filename
+                except self.EncryptedHeader:
+                    # The header is encrypted, try again
+                    # if it was our first attempt.
+                    if 0 == retry_count:
+                        continue
+            break
 
         self.filenames_initialized = True
 
     def extract(self, filename, destination_dir):
         """Extract <filename> from the archive to <destination_dir>"""
-        assert isinstance(filename, str) and isinstance(destination_dir, str)
+        assert isinstance(filename, str) and \
+               isinstance(destination_dir, str)
 
         if not self._get_executable():
             return
@@ -99,7 +142,8 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
             tmplistfile.write(filename + os.linesep)
             tmplistfile.flush()
             with self._create_file(destination_path) as output:
-                process.call(self._get_extract_arguments(list_file=tmplistfile.name), stdout=output)
+                process.call(self._get_extract_arguments(list_file=tmplistfile.name),
+                             stdout=output)
         return destination_path
 
     def iter_extract(self, entries, destination_dir):
@@ -128,7 +172,8 @@ class SevenZipArchive(archive_base.ExternalExecutableArchive):
 
     @staticmethod
     def _find_7z_executable():
-        """Try to start 7z. returns either '7z' or None"""
+        """Tries to start 7z, and returns either '7z' if
+        it was started successfully or None otherwise"""
         global _7z_executable
         if _7z_executable == -1:
             _7z_executable = process.find_executable('7z')

commit 9a00430b0992c5404e888287fec0a9418a7b046f

fix pageselect thumbnail preview
broken since 94f4fd0e5fd5034f335a61db2b1b88e5a63cfae3

mcomix/pageselect.py
@@ -112,7 +112,7 @@ class Pageselector(Gtk.Dialog):
                 self._generate_thumbnail, args=(page, width, height),
                 callback=self._generate_thumbnail_cb)
 
-    def _generate_thumbnail(self, params):
+    def _generate_thumbnail(self, page, width, height):
         """Generate the preview thumbnail for the page selector.
         A transparent image will be used if the page is not yet available"""
         return page, self._window.imagehandler.get_thumbnail(