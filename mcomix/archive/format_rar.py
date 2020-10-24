2f8fe5c7dc1b09cd03be44c91b336d910f1d6fd9
thermitegod <thermitethegod@gmail.com>
2020-10-24 12:08:56 -0700

    use unrar to extract rar files

--- mcomix/archive/format_rar.py
+++ mcomix/archive/format_rar.py
@@ -1,163 +1,69 @@
 # -*- coding: utf-8 -*-
 
-"""Glue around libunrar.so to extract RAR files without having to
-resort to calling rar/unrar manually"""
-
-import ctypes
-import ctypes.util
+import shutil
 from pathlib import Path
 
 from loguru import logger
 
 from mcomix.archive.archive_base import BaseArchive
-
-UNRARCALLBACK = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_uint, ctypes.c_long, ctypes.c_long, ctypes.c_long)
+from mcomix.lib import process
 
 
 class RarArchive(BaseArchive):
     """
-    Wrapper class for libunrar. All string values passed to this class must be unicode objects.
-    In turn, all values returned are also unicode
+    RAR file extractor using the unrar executable
     """
 
-    class _OpenMode:
-        """
-        Rar open mode
-        """
+    STATE_HEADER, STATE_LISTING = 1, 2
 
-        RAR_OM_EXTRACT = 1
+    def __init__(self, archive):
+        super().__init__(archive)
 
-    class _ProcessingMode:
-        """
-        Rar file processing mode
-        """
+        self.__unrar = RarExecutable.unrar_executable
 
-        RAR_SKIP = 0
-        RAR_EXTRACT = 2
+        self.__state = None
+        self.__path = None
 
-    class ErrorCode:
-        """
-        Rar error codes
-        """
-
-        ERAR_END_ARCHIVE = 10
-        ERAR_NO_MEMORY = 11
-        ERAR_BAD_DATA = 12
-        ERAR_BAD_ARCHIVE = 13
-        ERAR_UNKNOWN_FORMAT = 14
-        ERAR_EOPEN = 15
-        ERAR_ECREATE = 16
-        ERAR_ECLOSE = 17
-        ERAR_EREAD = 18
-        ERAR_EWRITE = 19
-        ERAR_SMALL_BUF = 20
-        ERAR_UNKNOWN = 21
-        ERAR_MISSING_PASSWORD = 22
-
-    class _RAROpenArchiveDataEx(ctypes.Structure):
-        """
-        Archive header structure. Used by DLL calls
-        """
-
-        _pack_ = 1
-        _fields_ = (
-            ('ArcName', ctypes.c_char_p),
-            ('ArcNameW', ctypes.c_wchar_p),
-            ('OpenMode', ctypes.c_uint),
-            ('OpenResult', ctypes.c_uint),
-            ('CmtBuf', ctypes.c_char_p),
-            ('CmtBufSize', ctypes.c_uint),
-            ('CmtSize', ctypes.c_uint),
-            ('CmtState', ctypes.c_uint),
-            ('Flags', ctypes.c_uint),
-            ('Callback', UNRARCALLBACK),
-            ('UserData', ctypes.c_long),
-            ('Reserved', ctypes.c_uint * 28),
-        )
-
-    class _RARHeaderDataEx(ctypes.Structure):
-        """
-        Archive file structure. Used by DLL calls
-        """
-
-        _pack_ = 1
-        _fields_ = (
-            ('ArcName', ctypes.c_char * 1024),
-            ('ArcNameW', ctypes.c_wchar * 1024),
-            ('FileName', ctypes.c_char * 1024),
-            ('FileNameW', ctypes.c_wchar * 1024),
-            ('Flags', ctypes.c_uint),
-            ('PackSize', ctypes.c_uint),
-            ('PackSizeHigh', ctypes.c_uint),
-            ('UnpSize', ctypes.c_uint),
-            ('UnpSizeHigh', ctypes.c_uint),
-            ('HostOS', ctypes.c_uint),
-            ('FileCRC', ctypes.c_uint),
-            ('FileTime', ctypes.c_uint),
-            ('UnpVer', ctypes.c_uint),
-            ('Method', ctypes.c_uint),
-            ('FileAttr', ctypes.c_uint),
-            ('CmtBuf', ctypes.c_char_p),
-            ('CmtBufSize', ctypes.c_uint),
-            ('CmtSize', ctypes.c_uint),
-            ('CmtState', ctypes.c_uint),
-            ('Reserved', ctypes.c_uint * 1024),
-        )
+        self.__contents = []
 
     @staticmethod
     def is_available():
-        """
-        Returns True if unrar.dll can be found, False otherwise
-
-        :returns: will return whether unrar is available
-        :rtype: bool
-        """
-
         return bool(RarExecutable.unrar_executable)
 
-    def __init__(self, archive: Path):
-        super().__init__(archive)
-        self.__unrar = RarExecutable.unrar_executable
-        self.__handle = None
-        # Information about the current file will be stored in this structure
-        self.__headerdata = RarArchive._RARHeaderDataEx()
-        self.__current_filename = None
+    def _get_list_arguments(self):
+        return [self.__unrar, 'vt', '--', self.archive]
 
-        # Set up function prototypes.
-        # Mandatory since pointers get truncated on x64 otherwise!
-        self.__unrar.RAROpenArchiveEx.restype = ctypes.c_void_p
-        self.__unrar.RAROpenArchiveEx.argtypes = [ctypes.POINTER(RarArchive._RAROpenArchiveDataEx)]
-        self.__unrar.RARCloseArchive.restype = ctypes.c_int
-        self.__unrar.RARCloseArchive.argtypes = [ctypes.c_void_p]
-        self.__unrar.RARReadHeaderEx.restype = ctypes.c_int
-        self.__unrar.RARReadHeaderEx.argtypes = [ctypes.c_void_p, ctypes.POINTER(RarArchive._RARHeaderDataEx)]
-        self.__unrar.RARProcessFileW.restype = ctypes.c_int
-        self.__unrar.RARProcessFileW.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_wchar_p, ctypes.c_wchar_p]
-        self.__unrar.RARSetCallback.argtypes = [ctypes.c_void_p, UNRARCALLBACK, ctypes.c_long]
+    def _get_extract_arguments(self):
+        return [self.__unrar, 'p', '-inul', '-@', '--', self.archive]
+
+    def _parse_list_output_line(self, line):
+        if self.__state == self.STATE_HEADER:
+            if line.startswith('Details: '):
+                self.__state = self.STATE_LISTING
+                return None
+        if self.__state == self.STATE_LISTING:
+            line = line.lstrip()
+            if line.startswith('Name: '):
+                self.__path = line[6:]
+                return self.__path
+            if line.startswith('Size: '):
+                filesize = int(line[6:])
+                if filesize > 0:
+                    self.__contents.append((self.__path, filesize))
+        return None
 
     def iter_contents(self):
-        """
-        List archive contents
-        """
-
-        self._close()
-        self._open()
-        try:
-            while 1:
-                self._read_header()
-                filename = self.__current_filename
-                yield filename
-                # Skip to the next entry if we're still on the same name
-                # (extract may have been called by iter_extract).
-                if filename == self.__current_filename:
-                    self._process()
-        except UnrarException as ex:
-            logger.error(f'Error while listing contents: {str(ex)}')
-        except EOFError:
-            # End of archive reached
-            pass
-        finally:
-            self._close()
+        #: Indicates which part of the file listing has been read.
+        self.__state = self.STATE_HEADER
+        #: Current path while listing contents.
+        self.__path = None
+        with process.popen(self._get_list_arguments(),
+                           stderr=process.STDOUT,
+                           universal_newlines=True) as proc:
+            for line in proc.stdout:
+                filename = self._parse_list_output_line(line.rstrip('\n'))
+                if filename is not None:
+                    yield filename
 
     def extract(self, filename: str, destination_dir: Path):
         """
@@ -171,134 +77,31 @@ class RarArchive(BaseArchive):
         :rtype: Path
         """
 
-        if not self.__handle:
-            self._open()
         destination_path = Path() / destination_dir / filename
-        while 1:
-            # Check if the current entry matches the requested file.
-            if self.__current_filename is not None:
-                if self.__current_filename == filename:
-                    # It's the entry we're looking for, extract it.
-                    dest = ctypes.c_wchar_p(str(destination_path))
-                    self._process(dest)
-                    break
-                # Not the right entry, skip it.
-                self._process()
-            try:
-                self._read_header()
-            except EOFError:
-                # Archive end was reached, this might be due to out-of-order
-                # extraction while the handle was still open.
-                logger.error('End of archive reached')
-                break
 
-        # After the method returns, the RAR handler is still open and pointing
-        # to the next archive file. This will improve extraction speed for sequential file reads.
-        # After all files have been extracted, close() should be called to free the handler resources.
+        with self._create_file(destination_path) as output:
+            process.call(self._get_extract_arguments(), stdout=output)
         return destination_path
 
-    def close(self):
-        """
-        Close the archive handle
-        """
+    def iter_extract(self, entries, destination_dir: Path):
+        with process.popen(self._get_extract_arguments()) as proc:
+            wanted = dict([(unicode_name, unicode_name) for unicode_name in entries])
 
-        self._close()
+            for filename, filesize in self.__contents:
+                data = proc.stdout.read(filesize)
+                if filename not in wanted:
+                    continue
+                unicode_name = wanted.get(filename, None)
+                if unicode_name is None:
+                    continue
 
-    def _open(self):
-        """
-        Open rar handle for extraction
-        """
-
-        archivedata = RarArchive._RAROpenArchiveDataEx(ArcNameW=self.archive,
-                                                       OpenMode=RarArchive._OpenMode.RAR_OM_EXTRACT,
-                                                       UserData=0)
-
-        handle = self.__unrar.RAROpenArchiveEx(ctypes.byref(archivedata))
-        if not handle:
-            errormessage = UnrarException.get_error_message(archivedata.OpenResult)
-            raise UnrarException(f'Could not open archive: {errormessage}')
-        self.__handle = handle
-
-    def _check_errorcode(self, errorcode: int):
-        """
-        Check rar error code to see if any exceptions should be raised
-
-        :param errorcode: rar error code
-        :type errorcode: int
-        """
-
-        if not errorcode:
-            # No error.
-            return
-        self._close()
-        if RarArchive.ErrorCode.ERAR_END_ARCHIVE == errorcode:
-            # End of archive reached.
-            exc = EOFError()
-        else:
-            errormessage = UnrarException.get_error_message(errorcode)
-            exc = UnrarException(errormessage)
-        raise exc
-
-    def _read_header(self):
-        self.__current_filename = None
-        errorcode = self.__unrar.RARReadHeaderEx(self.__handle, ctypes.byref(self.__headerdata))
-        self._check_errorcode(errorcode)
-        self.__current_filename = self.__headerdata.FileNameW
-
-    def _process(self, dest=None):
-        """
-        Process current entry: extract or skip it
-        """
-
-        if dest is None:
-            mode = RarArchive._ProcessingMode.RAR_SKIP
-        else:
-            mode = RarArchive._ProcessingMode.RAR_EXTRACT
-        errorcode = self.__unrar.RARProcessFileW(self.__handle, mode, None, dest)
-        self.__current_filename = None
-        self._check_errorcode(errorcode)
-
-    def _close(self):
-        """
-        Close the rar handle previously obtained by open
-        """
-
-        if self.__handle is None:
-            return
-        errorcode = self.__unrar.RARCloseArchive(self.__handle)
-        if errorcode != 0:
-            errormessage = UnrarException.get_error_message(errorcode)
-            raise UnrarException(f'Could not close archive: {errormessage}')
-        self.__handle = None
-
-
-class UnrarException(Exception):
-    """
-    Exception class for RarArchive
-    """
-
-    _exceptions = {
-        RarArchive.ErrorCode.ERAR_END_ARCHIVE: 'End of archive',
-        RarArchive.ErrorCode.ERAR_NO_MEMORY: 'Not enough memory to initialize data structures',
-        RarArchive.ErrorCode.ERAR_BAD_DATA: 'Bad data, CRC mismatch',
-        RarArchive.ErrorCode.ERAR_BAD_ARCHIVE: 'Volume is not valid RAR archive',
-        RarArchive.ErrorCode.ERAR_UNKNOWN_FORMAT: 'Unknown archive format',
-        RarArchive.ErrorCode.ERAR_EOPEN: 'Volume open error',
-        RarArchive.ErrorCode.ERAR_ECREATE: 'File create error',
-        RarArchive.ErrorCode.ERAR_ECLOSE: 'File close error',
-        RarArchive.ErrorCode.ERAR_EREAD: 'Read error',
-        RarArchive.ErrorCode.ERAR_EWRITE: 'Write error',
-        RarArchive.ErrorCode.ERAR_SMALL_BUF: 'Buffer too small',
-        RarArchive.ErrorCode.ERAR_UNKNOWN: 'Unknown error',
-        RarArchive.ErrorCode.ERAR_MISSING_PASSWORD: 'Password missing',
-    }
-
-    @staticmethod
-    def get_error_message(errorcode):
-        if errorcode in UnrarException._exceptions:
-            return UnrarException._exceptions[errorcode]
-
-        return 'Unkown error'
+                destination_path = Path() / destination_dir / unicode_name
+                with self._create_file(destination_path) as new:
+                    new.write(data)
+                yield unicode_name
+                del wanted[filename]
+                if not wanted:
+                    break
 
 
 class _RarExecutable:
@@ -316,12 +119,11 @@ class _RarExecutable:
         :returns: loaded unrar library
         """
 
-        unrar = ctypes.util.find_library('unrar')
+        unrar = shutil.which('unrar')
         if unrar is None:
-            logger.error(f'failed to find unrar library')
-            return None
+            logger.error(f'failed to find unrar executable')
 
-        return ctypes.cdll.LoadLibrary(unrar)
+        return unrar
 
 
 # Singleton instance
