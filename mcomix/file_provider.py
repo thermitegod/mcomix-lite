7510b5aba76634da81116bde8bdd6acd027e8684
thermitegod <thermitethegod@gmail.com>
2020-10-30 01:38:30 -0700

    Revert "simplify alphanumeric_sort()"
    This reverts commit 3438583292cdb8084961157874fc6011fe00f81a.

--- mcomix/file_provider.py
+++ mcomix/file_provider.py
@@ -14,6 +14,9 @@ from mcomix.constants import Constants
 from mcomix.image_tools import ImageTools
 from mcomix.preferences import config
 
+# Split into float, int, and characters
+NUMERIC_REGEXP = re.compile(r'\d+[.]\d+|\d+|\D+')
+
 
 class SortAlphanumeric:
     def __int__(self):
@@ -26,10 +29,19 @@ class SortAlphanumeric:
         such that for an example "1.jpg", "2.jpg", "10.jpg" is a sorted ordering
         """
 
+        def isfloat(p):
+            try:
+                return 0, float(p)
+            except ValueError:
+                return 1, p.lower()
+
         def keyfunc(s):
             x, y, z = str(s).rpartition('.')
-            # re.compile splits into float, int, and characters
-            return [p for p in (*re.compile(r'\d+[.]\d+|\d+|\D+').findall(x), z)]
+            if z.isdigit():
+                # extension with only digital is not extension
+                x = f'{x}{y}{z}'
+                z = ''
+            return [isfloat(p) for p in (*NUMERIC_REGEXP.findall(x), z)]
 
         filenames.sort(key=keyfunc)
 
