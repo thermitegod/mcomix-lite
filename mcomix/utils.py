dba737f9a3a9d0fd3312e0d9eda69a483323d522
thermitegod <thermitethegod@gmail.com>
2020-12-02 07:42:29 -0700

    remove repetitive casting to float

--- mcomix/utils.py
+++ mcomix/utils.py
@@ -8,17 +8,17 @@ class _Utils:
         super().__init__()
 
         if config['SI_UNITS']:
-            self.__unit_size = 1000
+            self.__unit_size = 1000.0
             self.__unit_symbols = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
         else:
-            self.__unit_size = 1024
+            self.__unit_size = 1024.0
             self.__unit_symbols = ('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB')
 
     def format_byte_size(self, n: int):
         s = 0
         while n >= self.__unit_size:
             s += 1
-            n /= float(self.__unit_size)
+            n /= self.__unit_size
 
         return f'{n:.3f} {self.__unit_symbols[s]}'
 
