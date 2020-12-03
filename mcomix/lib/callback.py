b07ebbcd867d4178318d12c2313d6e51a5dd7073
thermitegod <thermitethegod@gmail.com>
2020-12-03 06:32:01 -0700

    simplify callback.py
    in __get_function() the hasattr check would never evaluate True
    
    this resulted in the removal of a decent amount of dead code

--- mcomix/lib/callback.py
+++ mcomix/lib/callback.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import threading
-import weakref
 
 from gi.repository import GLib
 from loguru import logger
 
 
 class CallbackList:
-    """Helper class for implementing callbacks within the main thread.
-    Add listeners to method calls with method += callback_function"""
+    """
+    Helper class for implementing callbacks within the main thread.
+    Add listeners to method calls with method += callback_function
+    """
 
     def __init__(self, obj, function):
         super().__init__()
@@ -19,85 +20,56 @@ class CallbackList:
         self.__function = function
 
     def __call__(self, *args, **kwargs):
-        """Runs the wrapped function. After the funtion has finished,
+        """
+        Runs the wrapped function. After the funtion has finished,
         callbacks are run. Code within the function and the callback is
-        always executed in the main thread"""
-        if threading.currentThread().name == 'MainThread':
-            if self.__object:
-                # Assume that the Callback object is bound to a class method.
-                result = self.__function(self.__object, *args, **kwargs)
-            else:
-                # Otherwise, the callback should be bound to a normal function.
-                result = self.__function(*args, **kwargs)
+        always executed in the main thread
+        """
 
+        if threading.currentThread().getName() == 'MainThread':
+            # Assume that the Callback object is bound to a class method.
+            self.__function(self.__object, *args, **kwargs)
             self.__run_callbacks(*args, **kwargs)
-            return result
-
-        # Call this method again in the main thread.
-        GLib.idle_add(self.__mainthread_call, (args, kwargs))
+        else:
+            # Call this method again in the main thread.
+            GLib.idle_add(self.__mainthread_call, (args, kwargs))
 
     def __iadd__(self, function):
         """Support for 'method += callback_function' syntax"""
-        obj, func = self.__get_function(function)
 
-        if (obj, func) not in self.__callbacks:
-            self.__callbacks.append((obj, func))
+        if function not in self.__callbacks:
+            self.__callbacks.append(function)
 
         return self
 
     def __isub__(self, function):
         """Support for 'method -= callback_function' syntax"""
-        obj, func = self.__get_function(function)
 
-        if (obj, func) in self.__callbacks:
-            self.__callbacks.remove((obj, func))
+        if function in self.__callbacks:
+            self.__callbacks.remove(function)
 
         return self
 
     def __mainthread_call(self, params):
-        """Helper function to execute code in the main thread.
+        """
+        Helper function to execute code in the main thread.
         This will be called by GLib.idle_add, with <params> being a tuple
-        of (args, kwargs)"""
+        of (args, kwargs)
+        """
+
         self(*params[0], **params[1])
         # Removes this function from the idle queue
         return 0
 
     def __run_callbacks(self, *args, **kwargs):
         """Executes callback functions"""
-        for obj_ref, func in self.__callbacks:
-            if obj_ref is None:
-                # Callback is a normal function
-                callback = func
-            elif obj_ref() is not None:
-                # Callback is a bound method.
-                # Recreate it by binding the function to the object.
-                callback = func.__get__(obj_ref())
-            else:
-                # Callback is a bound method, object
-                # no longer exists.
-                callback = None
 
-            if callback:
-                try:
-                    callback(*args, **kwargs)
-                except Exception as ex:
-                    logger.error(f'Callback failed: {callback}')
-                    logger.error(f'Exception: {ex}')
-
-    def __callback_deleted(self, obj_ref):
-        """Called whenever one of the callback objects is collected by gc.
-        This removes all callback functions registered by the object"""
-        self.__callbacks = filter(lambda callback: callback[0] != obj_ref, self.__callbacks)
-
-    def __get_function(self, func):
-        """If <func> is a normal function, return (None, func).
-        If <func> is a bound method, return (weakref(obj), func), with <obj>
-        being the object <func> is bound to. This is required since
-        weak references do not work on bound methods"""
-        if hasattr(func, 'im_self') and getattr(func, 'im_self') is not None:
-            return weakref.ref(func.im_self, self.__callback_deleted), func.im_func
-
-        return None, func
+        for callback in self.__callbacks:
+            try:
+                callback(*args, **kwargs)
+            except Exception as ex:
+                logger.error(f'Callback failed: {callback}')
+                logger.error(f'Exception: {ex}')
 
 
 class Callback:
@@ -110,7 +82,10 @@ class Callback:
         self.__function = function
 
     def __get__(self, obj, cls):
-        """This method makes Callback implement the descriptor interface.
+        """
+        This method makes Callback implement the descriptor interface.
         Enables calling bound methods with the correct <self> reference.
-        Do not ask me why or how this actually works, I simply do not know"""
+        Do not ask me why or how this actually works, I simply do not know
+        """
+
         return CallbackList(obj, self.__function)
