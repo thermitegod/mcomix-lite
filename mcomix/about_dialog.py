405c487825c44a7fea2cdd434d41d55c06be0c06
thermitegod <thermitethegod@gmail.com>
2021-01-05 02:56:37 -0700

    update copyright date

--- mcomix/about_dialog.py
+++ mcomix/about_dialog.py
@@ -23,7 +23,7 @@ class AboutDialog(Gtk.AboutDialog):
         self.set_license_type(Gtk.License.GPL_2_0)
         self.set_website('https://github.com/thermitegod/mcomix-lite')
         self.set_website_label('Github')
-        self.set_copyright('Copyright (C) 2005-2020')
+        self.set_copyright('Copyright (C) 2005-2021')
 
         self.set_logo(Icons.load_icons())
 
