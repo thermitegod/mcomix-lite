91a0ace8953c9ad13bc5123b9206e6de4e6bab7e
thermitegod <thermitethegod@gmail.com>
2021-02-24 10:14:57 -0700

    mark as private

--- mcomix/file_chooser.py
+++ mcomix/file_chooser.py
@@ -31,35 +31,35 @@ class FileChooser(Gtk.Dialog):
         self.add_buttons(*(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
         self.set_default_response(Gtk.ResponseType.OK)
 
-        self.filechooser = Gtk.FileChooserWidget(action=self.__action)
-        self.filechooser.set_select_multiple(True)
-        self.filechooser.set_size_request(1280, 720)
-        self.vbox.pack_start(self.filechooser, True, True, 0)
+        self.__filechooser = Gtk.FileChooserWidget(action=self.__action)
+        self.__filechooser.set_select_multiple(True)
+        self.__filechooser.set_size_request(1280, 720)
+        self.vbox.pack_start(self.__filechooser, True, True, 0)
         self.set_border_width(4)
-        self.filechooser.set_border_width(6)
+        self.__filechooser.set_border_width(6)
         self.connect('response', self._response)
-        self.filechooser.connect('file_activated', self._response, Gtk.ResponseType.OK)
+        self.__filechooser.connect('file_activated', self._response, Gtk.ResponseType.OK)
 
         self.add_filter('All files', [])
         self.add_archive_filters()
         self.add_image_filters()
 
-        filters = self.filechooser.list_filters()
-        self.filechooser.set_filter(filters[config['FILECHOOSER_LAST_FILTER']])
+        filters = self.__filechooser.list_filters()
+        self.__filechooser.set_filter(filters[config['FILECHOOSER_LAST_FILTER']])
 
         try:
             current_file = self.__window.filehandler.get_path_to_base()
             if current_file is not None:
                 # If a file is currently open, use its path
-                self.filechooser.set_current_folder(str(current_file.parent))
+                self.__filechooser.set_current_folder(str(current_file.parent))
             elif self.__last_activated_file:
                 # If no file is open, use the last stored file
-                self.filechooser.set_filename(self.__last_activated_file)
+                self.__filechooser.set_filename(self.__last_activated_file)
             else:
                 # If no file was stored yet, fall back to preferences
-                self.filechooser.set_current_folder(config['FILECHOOSER_LAST_BROWSED_PATH'])
+                self.__filechooser.set_current_folder(config['FILECHOOSER_LAST_BROWSED_PATH'])
         except TypeError:
-            self.filechooser.set_current_folder(str(Path.home()))
+            self.__filechooser.set_current_folder(str(Path.home()))
 
         self.show_all()
 
@@ -77,7 +77,7 @@ class FileChooser(Gtk.Dialog):
                            self._filter, patterns)
 
         ffilter.set_name(name)
-        self.filechooser.add_filter(ffilter)
+        self.__filechooser.add_filter(ffilter)
         return ffilter
 
     def add_archive_filters(self):
@@ -87,7 +87,7 @@ class FileChooser(Gtk.Dialog):
 
         ffilter = Gtk.FileFilter()
         ffilter.set_name('All archives')
-        self.filechooser.add_filter(ffilter)
+        self.__filechooser.add_filter(ffilter)
 
         for ext in ArchiveTools.supported_archive_ext:
             ffilter.add_pattern(f'*{ext}')
@@ -99,14 +99,14 @@ class FileChooser(Gtk.Dialog):
 
         ffilter = Gtk.FileFilter()
         ffilter.set_name('All images')
-        self.filechooser.add_filter(ffilter)
+        self.__filechooser.add_filter(ffilter)
 
         for ext in ImageTools.supported_image_exts:
             ffilter.add_pattern(f'*{ext}')
 
     def files_chosen(self, paths: list):
         if paths:
-            filter_index = self.filechooser.list_filters().index(self.filechooser.get_filter())
+            filter_index = self.__filechooser.list_filters().index(self.__filechooser.get_filter())
             config['FILECHOOSER_LAST_FILTER'] = filter_index
 
             files = [Path(path) for path in paths]
@@ -132,12 +132,12 @@ class FileChooser(Gtk.Dialog):
         """
 
         if response == Gtk.ResponseType.OK:
-            if not self.filechooser.get_filenames():
+            if not self.__filechooser.get_filenames():
                 return
 
             # Collect files, if necessary also from subdirectories
             paths = []
-            for path in self.filechooser.get_filenames():
+            for path in self.__filechooser.get_filenames():
                 if Path.is_dir(Path(path)):
                     for file in Path(path).iterdir():
                         if ArchiveTools.is_archive_file(file) or ImageTools.is_image_file(file):
@@ -145,9 +145,9 @@ class FileChooser(Gtk.Dialog):
                 else:
                     paths.append(path)
 
-            self.__last_activated_file = self.filechooser.get_filenames()[0]
+            self.__last_activated_file = self.__filechooser.get_filenames()[0]
             self.files_chosen(paths)
 
-            config['FILECHOOSER_LAST_BROWSED_PATH'] = self.filechooser.get_current_folder()
+            config['FILECHOOSER_LAST_BROWSED_PATH'] = self.__filechooser.get_current_folder()
         else:
             self.files_chosen([])
