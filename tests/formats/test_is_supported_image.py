5489d8e02f4e3cfa9341bbaa8302b5f1cf125e57
thermitegod <thermitethegod@gmail.com>
2021-10-26 13:08:22 -0700

    add test for image/archive support

---
+++ tests/formats/test_is_supported_image.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pathlib import Path
+
+from mcomix.formats.image import ImageSupported
+
+
+@pytest.mark.parametrize('image', [
+    Path('test.jpeg'),
+    Path('test.jpg'),
+    Path('test.png'),
+
+    #Path('test.ppm'),
+    #Path('test.ppn'),
+])
+def test_is_archive(image: Path):
+    assert ImageSupported.is_image_file(image) == True
+
+@pytest.mark.parametrize('not_image', [
+    Path('test.pdf'),  # make sure .pdf files are not detected as supported
+
+    Path('test.not'),
+    Path('test.an'),
+    Path('test.image'),
+    Path('test.file'),
+])
+def test_is_not_archive(not_image: Path):
+    assert ImageSupported.is_image_file(not_image) == False
+
