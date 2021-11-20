ee6233b8b6518ee7b5a497111240b35fb929756e
thermitegod <thermitethegod@gmail.com>
2021-11-20 08:31:22 -0700

    add missing __slots__ value

--- mcomix/anime_framebuffer.py
+++ mcomix/anime_framebuffer.py
@@ -10,7 +10,7 @@ from mcomix.preferences import config
 
 
 class AnimeFrameBuffer:
-    __slots__ = ('__n_frames', '__width', '__height',
+    __slots__ = ('__n_frames', '__width', '__height', '__loop',
                  '__framelist', '__duration', '__fps')
 
     def __init__(self, n_frames: int, loop: int = 1):
