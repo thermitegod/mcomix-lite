176ddabc11f5beb7a3a976f477c583bfe7cbef04
thermitegod <43489010+thermitegod@users.noreply.github.com>
2026-02-14 12:53:04 -0700

    configure scripts for gcc and clang

---
+++ configure-clang.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CC=clang
+CXX=clang++
+LD=ld.lld
+# LLVM binutils
+AR=llvm-ar
+AS=llvm-as
+CPP=clang-cpp
+NM=llvm-nm
+STRIP=llvm-strip
+RANLIB=llvm-ranlib
+OBJCOPY=llvm-objcopy
+STRINGS=llvm-strings
+OBJDUMP=llvm-objdump
+READELF=llvm-readelf
+ADDR2LINE=llvm-addr2line
+
+meson setup \
+    -Dtests=true \
+    -Ddev=true \
+    -Duse-pixbuf-backend=true \
+    -Dwith-system-glaze=true \
+    -Dwith-system-spdlog=true \
+    -Dwith-system-ztd=true \
+    --buildtype=debug \
+    --prefix=$(pwd)/build ./build-clang
+
+# debug
+# debugoptimized
+
+# -Db_sanitize=undefined -Db_lundef=false \
+# -Db_sanitize=address -Db_lundef=false \
+# -Db_sanitize=address,undefined -Db_lundef=false \
---
+++ configure-gcc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CC=gcc
+CXX=g++
+LD=ld.bfd
+# binutils
+AR=ar
+AS=as
+CPP=cpp
+NM=nm
+STRIP=strip
+RANLIB=ranlib
+OBJCOPY=objcopy
+STRINGS=strings
+OBJDUMP=objdump
+READELF=readelf
+ADDR2LINE=addr2line
+
+meson setup \
+    -Dtests=true \
+    -Ddev=true \
+    -Duse-pixbuf-backend=true \
+    -Dwith-system-glaze=true \
+    -Dwith-system-spdlog=true \
+    -Dwith-system-ztd=true \
+    --buildtype=debug \
+    --prefix=$(pwd)/build ./build-gcc
+
+# debug
+# debugoptimized
+
+# -Db_sanitize=undefined -Db_lundef=false \
+# -Db_sanitize=address -Db_lundef=false \
+# -Db_sanitize=address,undefined -Db_lundef=false \
